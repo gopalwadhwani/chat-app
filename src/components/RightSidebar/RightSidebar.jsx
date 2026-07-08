94900f39928626b3f1468092cec9c8438feab01a
gopal wadhwani <gopalwadhwani435@gmail.com>
2026-07-08 16:15:15 +0530

    added functionality to rightsidebar

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './RightSidebar.css'
 import assets from '../../assets/assets'
-import { logout } from '../../config/firebase' // adjust path to your firebase config file
+import { logout, db } from '../../config/firebase'
+import { AppContext } from '../../context/AppContext'
+import { doc, onSnapshot } from 'firebase/firestore'
 
 const RightSidebar = () => {
+    const { chatUser, messagesId } = useContext(AppContext)
+    const [mediaMessages, setMediaMessages] = useState([])
+
+    useEffect(() => {
+        if (!messagesId) {
+            setMediaMessages([])
+            return
+        }
+
+        const unsubscribe = onSnapshot(doc(db, "messages", messagesId), (docSnap) => {
+            const data = docSnap.data()
+            const images = (data?.messages || []).filter((m) => m.image)
+            setMediaMessages(images)
+        })
+
+        return () => unsubscribe()
+    }, [messagesId])
+
+    if (!chatUser) {
+        return (
+            <div className='rs'>
+                <button onClick={() => logout()}>Logout</button>
+            </div>
+        )
+    }
+
     return (
         <div className='rs'>
             <div className="rs-profile">
-                <img src={assets.profile_img} alt="" />
+                <img src={chatUser.userData.avatar || assets.profile_img} alt="" />
                 <h3>
-                    Richard Sanford
+                    {chatUser.userData.name || chatUser.userData.username}
                     <img src={assets.green_dot} className='dot' alt="" />
                 </h3>
-                <p>Hey, There i am Richard Sanford using chat app</p>
+                <p>{chatUser.userData.bio || "Hey, there! I am using chat app"}</p>
             </div>
 
             <hr />
@@ -20,12 +48,18 @@ const RightSidebar = () => {
             <div className="rs-media">
                 <p>Media</p>
                 <div>
-                    <img src={assets.pic1} alt="" />
-                    <img src={assets.pic2} alt="" />
-                    <img src={assets.pic3} alt="" />
-                    <img src={assets.pic4} alt="" />
-                    <img src={assets.pic1} alt="" />
-                    <img src={assets.pic2} alt="" />
+                    {mediaMessages.length > 0 ? (
+                        mediaMessages.map((msg, index) => (
+                            <img
+                                key={index}
+                                src={msg.image}
+                                alt=""
+                                onClick={() => window.open(msg.image, "_blank")}
+                            />
+                        ))
+                    ) : (
+                        <p className="no-media">No media shared yet</p>
+                    )}
                 </div>
             </div>
 
